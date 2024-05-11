8d3b6acc2cb4476c1b009ac9812d8be1fd8a4c90
Marble <scnideffer@gmail.com>
2024-05-10 22:32:04 -0700

    added finishing touches, submitting

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
+/*
+Lab 5
+Name: Scott "Marble" Nideffer
+Date: 05/10/2024
+IDE: Clion
+Version: 1.0
+Description: A program that creates student struct arrays with Firstname Lastname Lnumber and GPA, sorts them, and allows for
+ a search of 5 students.
+*/
 #include <iostream>
-
 #include "helper.hpp"
 #include "required.hpp"
 
-// It will then use std::sort to sort the array by lastName and call displayArray
-// again to show the new sorting.
-
-
-// Finally it will loop five times getting a lastName,
-// searching the array for that name, and, if the name is present, displaying the firstName,
-// lastName and GPA.
-
 int main(){
     int searches = 0;
+    // gets number of students
     int numStds = getInteger(1,20);
+    // creates student struct array
     Student *students = createArray(numStds);
     std::cout << "Number of students: " << numStds << "." << std::endl;
-    // DisplayArrays sorted by LNum, then displays sorted by last
+    // displays arrays sorted by Lnumber then by lastname
     displayArrays(students, numStds);
-    // Search for 5 students by Last name
-    // display first, last, GPA
     do {
+        // allows for 5 lastname searches
         std::string query = getSearchLast();
         binSearch(students, query, numStds);
         searches++;
         std::cout << searches << " searches of 5 used." << std::endl;
     } while (searches < 5);
-
 
     std::cout << "Thank you for using Nideffer Industries' Student Lookup Program." << std::endl;
 
